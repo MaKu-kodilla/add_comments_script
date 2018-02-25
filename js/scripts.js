4f4e79f063e6a1b47a1b5cb1e2e8e23210e4807e
Maciej Kubas <maciekkubas@gmail.com>
2018-02-25 22:35:47 +0100

    created project structure & added comments to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+// This is LINEAR comment! 
+// var name = prompt('Enter your name');
+
+// This is another LINEAR comment!
+// alert('Hello, ' + name);
+
+/*
+
+This is BLOCK comment
+console.log('Hello, ' + name);
+
+*/
